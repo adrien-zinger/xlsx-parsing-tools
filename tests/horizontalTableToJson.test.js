78099810211fa91656807e6c94b8b59a9ec418f0
Adrien Zinger <zinger.ad@gmail.com>
2021-06-21 18:12:24 +0200

    Avoid duplication lines

--- tests/horizontalTableToJson.test.js
+++ tests/horizontalTableToJson.test.js
@@ -38,3 +38,13 @@ test("Testing horizintal table style to json 3", () => {
   const ret = parser(table, { headAdapter })
   expect(ret.dodo["ligne1"]).toEqual("vl1a")
 })
+
+test("Testing horizintal table style to json 4", () => {
+  //const table = [
+  //  ["", "ligne1", "ligne2", "ligne2"],
+  //  ["a", "vl1a", "vl2a", "te"],
+  //  ["b", "vl1b", "vl2b", "te"],
+  //]
+  //const ret = parser(table)
+  //expect(ret).toEqual('{"a":{"ligne1":"vl1a","ligne2":"vl2a","ligne22":"te"},"b":{"ligne1":"vl1b","ligne2":"vl2b","ligne22":"te"}}')
+})
