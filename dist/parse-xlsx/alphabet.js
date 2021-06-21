d262af2686a71199b8451a88fb7223ad3d1f9aed
Adrien Zinger <azinger@alpenite.com>
2021-06-21 14:21:36 +0200

    fix regenerator

--- dist/parse-xlsx/alphabet.js
+++ dist/parse-xlsx/alphabet.js
@@ -1,19 +1,23 @@
 "use strict";
 
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.getAlphabetSerie = getAlphabetSerie;
 exports.alphabet = void 0;
 
-var _marked = /*#__PURE__*/regeneratorRuntime.mark(gen);
+var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
+
+var _marked = /*#__PURE__*/_regenerator["default"].mark(gen);
 
 var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "X", "Y", "Z"];
 exports.alphabet = alphabet;
 
 function gen(start) {
   var index, alen;
-  return regeneratorRuntime.wrap(function gen$(_context) {
+  return _regenerator["default"].wrap(function gen$(_context) {
     while (1) {
       switch (_context.prev = _context.next) {
         case 0:
--- dist/parse-xlsx/index.js
+++ dist/parse-xlsx/index.js
@@ -1,10 +1,14 @@
 "use strict";
 
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.xlsxParser = xlsxParser;
 exports.xlsxMdPrint = xlsxMdPrint;
+
+var _slicedToArray2 = _interopRequireDefault(require("@babel/runtime/helpers/slicedToArray"));
 
 var _xlsx = _interopRequireDefault(require("xlsx"));
 
@@ -12,21 +16,11 @@ var _alphabet = require("./alphabet.js");
 
 var _config2 = _interopRequireDefault(require("./config.js"));
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e2) { throw _e2; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e3) { didErr = true; err = _e3; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
-
-function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
-
-function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
 
 function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
 
 function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
-
-function _iterableToArrayLimit(arr, i) { var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"]; if (_i == null) return; var _arr = []; var _n = true; var _d = false; var _s, _e; try { for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
-
-function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
 /**
  *
@@ -37,7 +31,7 @@ function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
  * @returns
  */
 function graps(worksheet, _ref, map) {
-  var _ref2 = _slicedToArray(_ref, 4),
+  var _ref2 = (0, _slicedToArray2["default"])(_ref, 4),
       letter_start = _ref2[0],
       letter_end = _ref2[1],
       num_start = _ref2[2],
@@ -90,7 +84,7 @@ function xlsxMdPrint(table, cb) {
 
   out += "\n|";
 
-  for (var _i2 = 0; _i2 < width; ++_i2) {
+  for (var _i = 0; _i < width; ++_i) {
     out += " ------ |";
   }
 
@@ -137,13 +131,13 @@ function removeEmpties(table) {
     if (_rm) column_to_remove.push(_y);
   }
 
-  for (var _i3 = 0, _column_to_remove = column_to_remove; _i3 < _column_to_remove.length; _i3++) {
-    var c = _column_to_remove[_i3];
+  for (var _i2 = 0, _column_to_remove = column_to_remove; _i2 < _column_to_remove.length; _i2++) {
+    var c = _column_to_remove[_i2];
     table.splice(c, 1);
   }
 
-  for (var _i4 = 0, _lines_to_remove = lines_to_remove; _i4 < _lines_to_remove.length; _i4++) {
-    var l = _lines_to_remove[_i4];
+  for (var _i3 = 0, _lines_to_remove = lines_to_remove; _i3 < _lines_to_remove.length; _i3++) {
+    var l = _lines_to_remove[_i3];
 
     var _iterator2 = _createForOfIteratorHelper(table),
         _step2;
