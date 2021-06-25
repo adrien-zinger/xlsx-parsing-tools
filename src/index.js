3bef95a07edd429624cf3f4c30b2f88b1dec5823
Adrien Zinger <azinger@alpenite.com>
2021-06-25 12:16:11 +0200

    prettify code source

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import { xlsxParser, xlsxMdPrint } from "./parse-xlsx/index.js"
-import expanseDot from "./json-cleaner/expanse-dots.js"
+import { xlsxParser, xlsxMdPrint } from './parse-xlsx/index.js'
+import expanseDot from './json-cleaner/expanse-dots.js'
 import hTableToJson from './parse-xlsx/horizontalStyle'
 
 const cleaner = {
